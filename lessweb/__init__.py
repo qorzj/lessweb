649841cee13d096fb839f69e0a9a0b1f97243a74
zhangji <goodhorsezxj@gmail.com>
2018-03-12 14:14:26 +0800

    fix bug

--- lessweb/__init__.py
+++ lessweb/__init__.py
@@ -14,7 +14,7 @@ from . import application, context, model, storage, webapi
 
 from .application import interceptor, Application
 from .context import Context
-from .model import rest_param, need_param, choose_param, unchoose_param, tips, get_tips, get_annotations
+from .model import rest_param, tips, get_tips, get_annotations
 from .model import get_func_parameters, get_model_parameters, Model
 from .model import enum_show, Enum
 from .storage import Storage
