d751e33d110bdfefbabf5bd98ced86d70a8e756b
zhangji <zhangji@parsec.com.cn>
2021-02-08 09:40:01 +0800

    simplify and complete xml_from_params

--- lessweb/libs/paramxml.py
+++ lessweb/libs/paramxml.py
@@ -13,35 +13,18 @@ def make_node(key, value):
     return node
 
 
-def array_startswith(arr, prefix_arr):
-    prefix_len = len(prefix_arr)
-    return len(arr) >= prefix_len and arr[:prefix_len] == prefix_arr
-
-
 def xml_from_params(params: str, root_name='data', encoding='utf-8') -> Element:
     root = Element(root_name)
     param_segs = params.split('&')
-    stack = []  # (key, node)
     for param_seg in param_segs:
         param_key, param_value = param_seg.split('=', 1) if '=' in param_seg else (param_seg, '')
-        param_key = urllib.parse.unquote(param_key)
-        param_value = urllib.parse.unquote(param_value)
-        param_key_segs = param_key.split('.')
-        if not param_key_segs or any(not(seg) for seg in param_key_segs):
-            raise ParseError('empty param key')
+        param_key = urllib.parse.unquote(param_key, encoding=encoding)
+        param_value = urllib.parse.unquote(param_value, encoding=encoding)
         node = make_node(param_key, param_value)
-        print(stack, param_key, param_value)
-        if stack and array_startswith(param_key_segs, stack):
-            stack[-1][1].append(node)
-        elif len(param_key_segs) == len(stack) + 1:
-            stack.append((param_key_segs[-1], node))
-        elif len(param_key_segs) == len(stack):
-            stack[-1] = (param_key_segs[-1], node)
-        else:
-            raise ParseError('parse failed')
+        root.append(node)
     return root
 
 
 if __name__ == '__main__':
-    params = 'book&book.title=Everyday%20Italian&book.author=Giada&book.price=30.00&book&book.title=Harry%20Potter&book.author=Rowling&book.price=29.99'
-    xml_from_params(params)
+    params = 'title=Everyday%20Italian&author=Giada&price=30.00&title=Harry%20Potter&author=Rowling&price=29.99'
+    ret = xml_from_params(params)  # <data><title>Everyday Italian</title><author>Giada</author><price>30.00</price><title>Harry Potter</title><author>Rowling</author><price>29.99</price></data>
